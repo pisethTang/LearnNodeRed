b7ca965ba185eb8b4f4a82981b7e17a2457a74ce
Tang Piseth <tangpiseth43@gmail.com>
2026-06-15 16:04:19 +0930

    feat: production deployment on AWS EC2 with systemd services

--- sensor-api/main.go
+++ sensor-api/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "/tmp/sensor_log.db")
+	db, err := sql.Open("sqlite3", "/home/ubuntu/sensor_log.db")
 	if err != nil {
 		log.Fatalf("open db: %v", err)
 	}
